92d3c0d50afdb64b2e31bc4349004acd468b07e2
Daniel David <danidvd821@gmail.com>
2023-03-16 12:19:30 -0500

    Esta es la version 2 de la carta de Gastos SOAT

---
+++ funciones.js
@@ -0,0 +1,40 @@
+
+// Funcion para mostrar nuevo elemento cuando fecha de ingreso a la institucion es diferente a la fecha del accidente
+
+function mostrarFechaIngreso(){
+    const newElement = document.createElement("div");
+    newElement.classList.add("divElemento2");
+    newElement.innerHTML = 
+    `<label "width:100%">
+    <div style="width:20%;margin-right:1rem">
+    <span><strong>Fecha Siniestro</strong></span>
+    </div>
+    <div style="width:80%">
+    <input type="text" class="cartaDiaIngreso2" maxlength="2" name="" id="InputCartaDia2" list="diaAT" placeholder="Día" style="width: 6rem">
+    <input type="text" class="cartaMesIngreso2" id="InputCartaMes2" maxlength="2" list="mesAT" placeholder="Mes" style="width: 6rem">
+    <input type="text" class="annioCartaIngreso2" maxlength="4" id="InputCartaAnnio2" list="annioAT" placeholder="Año" style="width: 8rem">
+    </div>
+    </label>`
+    document.querySelector(".container").appendChild(newElement);
+};
+
+
+// Funcion para mostrar nuevo elemento cuando es por tope partido entre asegurador y EPS
+
+function mostrarSegundoTope(){
+    const newElement = document.createElement("div");
+    newElement.classList.add("div");
+    newElement.innerHTML = `<div class="concept-left">
+    <label>
+    <input type="text" class="totalGasto2" name="" maxlength="8" id="InputTotalGastoDos" placeholder="Total gasto N°2" style="width: 22rem;height: auto;">
+    </label>
+    </div>`
+    document.querySelector(".container2").appendChild(newElement);
+};
+
+var respuesta1 = "SI"
+var respuesta2 = "NO"
+
+
+
+
---
+++ index.js
@@ -0,0 +1,186 @@
+
+function onclickSumarNombre(){
+
+    //Me muestra nombre de usuario
+    var NameUserInput = document.getElementById("InputNameUser");
+    var NameUser = NameUserInput.value;
+
+    //Me muestra tipo de documento
+    var tipoIdentificacionInput = document.getElementById("InputTiPaciente");
+    var tipoIdentificacion = tipoIdentificacionInput.value;
+
+    //Me muestra la cedula
+    var documentoPacienteInput = document.getElementById("InputDocumento");
+    var documentoPaciente = documentoPacienteInput.value;
+    var documentoPacienteCompleto = formatoMexico(documentoPaciente);
+
+    // fecha accidente (FUNCION SI)
+    var respuestaSiNo1Input = document.getElementById("ingresoInputAccidente2");
+    var respuestaSiNo1 = respuestaSiNo1Input.value;
+    var Parrafo1Final = mostrarRespuestaFechaIngreso(respuestaSiNo1)
+
+    // fecha ingreso a la institucion accidente
+    var diaInput = document.getElementById("Inputdia");
+    var dia = diaInput.value;
+
+    var mesInput = document.getElementById("InputMes");
+    var mes = mesInput.value;
+    var mesInputAccidente = mostrarNombreMes(mes)
+
+    var annioInput = document.getElementById("InputAnnio");
+    var annio = annioInput.value;
+
+    var aseguradorInput = document.getElementById("InputAsegurador");
+    var asegurador = aseguradorInput.value;
+
+    // gasto
+
+    var gastoInput = document.getElementById("InputTotalGasto");
+    var gasto = gastoInput.value;
+    var gastoTotal = formatoMexico(gasto);
+
+    // Gasto 2 tope partido
+
+    var gasto2Input = document.getElementById("InputTotalGastoDos");
+    var gasto2 = gasto2Input.value;
+    var gastoTotal2 = formatoMexico(gasto2);
+    var gastoTotalReal = mostrarTopeCarta(gasto2)
+
+    // nota u observacion
+
+    var notaInput = document.getElementById("InputNota");
+    var nota = notaInput.value;
+
+    // fecha carta
+
+    var cartaDiaInput = document.getElementById("InputCartaDia");
+    var cartaDia = cartaDiaInput.value;
+
+    var cartaMesInput = document.getElementById("InputCartaMes");
+    var cartaMes = cartaMesInput.value;
+    var cartaMesAccidente = mostrarNombreMes(cartaMes);
+
+    var cartaAnnioInput = document.getElementById("InputCartaAnnio");
+    var CartaAnnio = cartaAnnioInput.value;
+
+    //Funcionario
+
+    var funcionarioInput = document.getElementById("InputFuncionario");
+    var funcionaro = funcionarioInput.value;
+    var nombreEmpleados = mostrarNombreUsuario(funcionaro);
+    var nombreCargo = mostrarNombreCargo(funcionaro);
+    var nombreCorreo = mostrarNombreCorreo(funcionaro);
+
+    //Motivo
+
+    var motivoCartaInput = document.getElementById("motivo-Input");
+    var motivoCarta = motivoCartaInput.value;
+    var queMotivoCarta = mostrarMotivo(motivoCarta);
+    var CualMotivoCarta = mostrarMotivoFinal(motivoCarta);
+
+    //Si ingreso es diferente a fecha accidente
+
+    // Se Muestran los cambios en la carta Parrafo1
+
+    var ResultadoNombre = document.getElementById("ResultadoNombre");
+    ResultadoNombre.innerText = Parrafo1Final;
+
+    var ResultadoAccidente = document.getElementById("ResultadoAccidente");
+    ResultadoAccidente.innerText = "Desde el momento del ingreso a la institución el día " + dia + " de " + mesInputAccidente + " " + annio + " ha generado gastos médicos a cargo de: " + asegurador;
+
+    var ResultadoGasto = document.getElementById("ResultadoGasto");
+    ResultadoGasto.innerText = "$ " + gastoTotal;
+
+    var ResultadoGasto2 = document.getElementById("ResultadoGasto2");
+    ResultadoGasto2.innerText = gastoTotalReal;
+
+    var ResultadoCarta = document.getElementById("ResultadoCarta");
+    ResultadoCarta.innerText = "Esta constancia se realiza a solicitud del usuario y se firma en la ciudad de Medellín a los " + cartaDia + " días del mes de " + cartaMesAccidente + " de " + CartaAnnio;
+
+    var ResultadoNota = document.getElementById("ResultadoNota");
+    ResultadoNota.innerText = CualMotivoCarta;
+
+    var ResultadoEmpleado = document.getElementById("ResultadoEmpleado");
+    ResultadoEmpleado.innerText = nombreEmpleados;
+
+    var ResultadoCargo = document.getElementById("ResultadoCargo");
+    ResultadoCargo.innerText = nombreCargo;
+
+    var ResultadoCorreo = document.getElementById("ResultadoCorreo");
+    ResultadoCorreo.innerText = nombreCorreo;
+
+    function mostrarMotivoFinal(motivo){
+        var servicioInput = document.getElementById("inputServicio");
+        var servicio = servicioInput.value;
+        if(motivo === motivo1){
+            return "NOTA: " + "Paciente solicita corte de cuenta para " + servicio + queMotivoCarta
+        }
+        if(motivo === motivo2){
+            return "NOTA: " + "Paciente solicita corte de cuenta para " + servicio + queMotivoCarta
+        }
+        if(motivo === motivo3){
+            return "NOTA: " + "Paciente solicita corte de cuenta para " + servicio + queMotivoCarta
+        }
+        if(motivo === motivo4){
+            return "NOTA: " + queMotivoCarta
+        }
+        if(motivo === motivo5){
+            return "NOTA: " + queMotivoCarta
+        }
+        if(motivo === motivo6){
+            return "NOTA: " + queMotivoCarta
+        }
+        if(motivo === motivo7){
+            return "NOTA: " + nota;
+        }
+    };
+
+    function mostrarRespuestaFechaIngreso(respuesta){
+
+        var ingresoDiaInput = document.getElementById("InputCartaDia2");
+        var ingresoDia = ingresoDiaInput.value;
+        
+        var ingresoMesInput = document.getElementById("InputCartaMes2");
+        var ingresoMes = ingresoMesInput.value;
+        var ingresoMesAccidente = mostrarNombreMes(ingresoMes);
+        
+        var ingresoAnnioInput = document.getElementById("InputCartaAnnio2");
+        var ingresoAnnio = ingresoAnnioInput.value;
+
+        if(respuesta===respuesta1){
+            return "Certifica que " + NameUser  + " identificado con " + tipoIdentificacion + " " + documentoPacienteCompleto + " ingresó a esta institución tras sufrir un accidente de tránsito, el día " + ingresoDia + " de "+ ingresoMesAccidente + " " +ingresoAnnio +".";
+        };
+        if(respuesta===respuesta2){
+            return "Certifica que " + NameUser  + " identificado con " + tipoIdentificacion + " " + documentoPacienteCompleto + " ingresó a esta institución tras sufrir un accidente de tránsito.";
+        };
+    };
+
+    function mostrarTopeCarta(valor){
+        if(valor>0){
+            return "$ " + gastoTotal2;
+        }
+        if(valor<=0){
+            return "";
+        };
+    };
+};
+
+
+const formatoMexico = (number) => {
+    const exp = /(\d)(?=(\d{3})+(?!\d))/g;
+    const rep = '$1,';
+    return number.toString().replace(exp,rep);
+  }
+
+  function imprimCartaGastos(){
+    var printContents = document.getElementById('CartaGasto').innerHTML;
+            w = window.open();
+            w.document.write(printContents);
+            w.document.close(); // necessary for IE >= 10
+            w.focus(); // necessary for IE >= 10
+        w.print();
+        w.close();
+            return true;}
+
+
+
